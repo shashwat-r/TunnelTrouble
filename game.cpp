a0eb39e9c9966c019937dd1895ce7868ba83f31a
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2018-03-16 03:18:47 +0530

    Tunnel view + number control

--- game.cpp
+++ game.cpp
@@ -3,6 +3,7 @@
 #include<GL/glut.h>
 #include<vector>
 #include<deque>
+#include<map>
 using namespace std;
 
 float globalvar = 0.3, globaldiff = 0.1; // for debugging purposes only
@@ -13,33 +14,34 @@ void timer(int)
 {
     /* update animation */
     for(int i = 0; i < dq.size(); i++) {
-        dq[i] += 0.3;
+        dq[i] += 0.1;
     }
     // for(int i = 0; i < dq.size(); i++) {
     //     cout<<dq[i]<<" ";
     // }
     // cout<<endl;
-    while(dq[2] >= 10.0) {
-        dq.push_back(dq[dq.size() - 1] - 1 - 10);
+    while(dq[0] >= 25.0) {
+        dq.push_back(dq[dq.size() - 1] - 1 - 1);
         dq.pop_front();
     }
     glutPostRedisplay();
-    glutTimerFunc(1000.0/20.0, timer, 0);
+    glutTimerFunc(1000.0/60.0, timer, 0);
 }
-
+map<char, bool> isPressed;
 void handleKeypress(unsigned char key, int x, int y) {    //The current mouse coordinates
     switch (key) {
         case 27: //Escape key
             exit(0);
         break;
         case 's':
-            timer(0);
+            if(!isPressed['s']) timer(0);
+            isPressed['s'] = 1;
         break;
         case 'a':
-            initAngle += 5;
+            isPressed['a'] = 1;
         break;
         case 'd':
-            initAngle -= 5;
+            isPressed['d'] = 1;
         break;
         case 'm':
             globalvar += globaldiff;
@@ -50,7 +52,17 @@ void handleKeypress(unsigned char key, int x, int y) {    //The current mouse co
     }
     glutPostRedisplay();
 }
-
+void handleKeyrelease(unsigned char key, int x, int y) {    //The current mouse coordinates
+    switch (key) {
+        case 'a':
+            isPressed['a'] = 0;
+        break;
+        case 'd':
+            isPressed['d'] = 0;
+        break;
+    }
+    glutPostRedisplay();
+}
 void wall(float xWidth, float yThickness, float zLength) {
     glPushMatrix();
     glTranslatef(0, yThickness/2, 0);
@@ -62,11 +74,11 @@ void wall(float xWidth, float yThickness, float zLength) {
 void octagonRoom(float zPos, float initAngle) {
     int sides = 16;
     float angle = 360.0/(float)sides;
-    float length = 5.0;
+    float length = 1.0;
     float thickness = 0.05;
     float width = 0.5;
     float radius = (width/2)/tan((angle/2)*(3.14159/180));
-    float obstacleHeight = 0.3;
+    float obstacleHeight = globalvar;
     vector<bool> obstacleExists(sides, false);
     obstacleExists[0] = obstacleExists[4] = 1;
     for(int i = 0; i < sides; i++) {
@@ -86,19 +98,23 @@ void octagonRoom(float zPos, float initAngle) {
                 glRotatef(-angle*i + initAngle, 0.0, 0.0, 1.0);
                 glTranslatef(0.0, radius - obstacleHeight, zPos);
                 float colorinput = (i%2+1)*0.2;
-                glColor3f(colorinput, colorinput, colorinput);
+                glColor3f(1.0, 0.0, 0.0);
                 wall(width, thickness + obstacleHeight, length);
             glPopMatrix();
         }
     }
 }
 
-void displaySolid() {
+void draw() {
+    if(isPressed['a']) initAngle += 1.0;
+    if(isPressed['d']) initAngle -= 1.0;
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, 1.333, 0.01, 100);
+    gluLookAt(0.0, -1.0, 20.0,       0.0, 0.0, 0.0,     0.0, 1.0, 0.0);
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -107,20 +123,24 @@ void displaySolid() {
     }
 
     glFlush();
-    gluLookAt(0.0, 0.0, -20.0,       0.0, 0.0, 0.0,     0.0, 1.0, 0.0);
 }
 
 int main(int argc,char **argv)
 {
-    dq.push_back(10); dq.push_back(-1); dq.push_back(-12);
+    for(int i = 0; i < 10; i++) {
+        dq.push_back((float)(-2.0*i));
+    }
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
     glutInitWindowSize(640,480);
     glutInitWindowPosition(0,0);
     glutCreateWindow("Tunnel Trouble");
-    glutDisplayFunc(displaySolid);
+    glutDisplayFunc(draw);
     glutKeyboardFunc(handleKeypress);
+    glutKeyboardUpFunc(handleKeyrelease);
+
     glEnable(GL_NORMALIZE);
+    glEnable(GL_DEPTH_TEST);
 
     glClearColor(0.1,0.1,0.1,0.0);
     glViewport(0,0,640,480);
