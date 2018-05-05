af12b88b0c647e48de167ff273ca653256a037b8
Dhruv Chadha <cse150001009@iiti.ac.in>
2018-05-05 13:08:03 +0530

    Gliding tunnel, adjusting with viewer

--- oldIterations/Game/shapes.h
+++ oldIterations/Game/shapes.h
@@ -4,12 +4,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-float globalZ = 0, division = 0.0;
-float framerate = 120.0;
-float eye[3] = {0.0, 10.0, 7.0}, center[3] = {0.0, 0.0, -20.01}, up[3] = {0.0, 1.0, 0.0};
+float globalZ = 0;
+
+float eye[3] = {0.0, 0.0, 0.0}, center[3] = {0.0, 0.0, -0.01}, up[3] = {0.0, 1.0, 0.0};
 
 void draw();
-float lx = 0.0, ly = 0.0, lz = 15, zLength = 2.0, tunnelAngle = 0.0, piby180 = 3.14/180;
+float lx = 0.0, ly = 0.0, lz = 15, zLength = 1, tunnelAngle = 0.0, piby180 = 3.14/180;
 std::deque<pair<float, float> > dq;
 map<char, bool> isPressed;
 
@@ -25,7 +25,7 @@ void handleKeyrelease(unsigned char key, int x, int y) {
 }
 
 void wall(float xWidth, float zLength = 100) {
-    float miniSquares = 1;
+    float miniSquares = 5;
     for(float z = -zLength/2; z < zLength/2; z += zLength/miniSquares) {
         for(float x = -xWidth/2; x < xWidth/2; x += xWidth/miniSquares) {
             glBegin(GL_QUADS);
@@ -53,10 +53,7 @@ void room() {
 }
 
 float tunnelCurve(float zPos) {
-    float stretch = 1000;
-    float radToDeg = zPos/piby180;
-    float amplitude = 2;
-    return amplitude * sin( radToDeg / stretch );
+    return 2*sin(zPos/(piby180 * 1000));
     // return zPos/20;
 }
 
@@ -67,19 +64,17 @@ float tunnelDerivativeCurve(float zPos) {
 
 void timer(int)
 {
-	division += 0.1;
     for(int i = 0; i < dq.size(); i++) {
         dq[i].first += 0.1;
     }
     if(dq[0].first >= 0.0) {
-        division = 0.0;
         float zPos = dq[dq.size() - 1].first - zLength;
         dq.push_back( {zPos, tunnelCurve(globalZ)} );
         dq.pop_front();
         globalZ -= zLength;
     }
     glutPostRedisplay();
-    glutTimerFunc(1000.0/framerate, timer, 0);
+    glutTimerFunc(1000.0/120.0, timer, 0);
 }
 
 void initEnv() {
